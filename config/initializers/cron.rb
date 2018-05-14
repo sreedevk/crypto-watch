44988934eb76a7f1b19880e22992437af4b8e45e
Sreedev Kodichath <sreedev@icloud.com>
2018-05-14 08:00:52 +0530

    cron fixes

--- config/initializers/cron.rb
+++ config/initializers/cron.rb
@@ -1,7 +1,5 @@
-Sidekiq::Cron::Job.destroy_all!
-
+schedule_file = File.join(Rails.root, "config", "cron.yml")
 if File.exist?(schedule_file) && Sidekiq.server?
-  schedule_file = File.join(Rails.root, "config", "cron.yml")
   Sidekiq::Cron::Job.destroy_all!
   Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
 end
